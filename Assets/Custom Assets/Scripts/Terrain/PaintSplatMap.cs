60cf9d261aa68fe449682d4f8849d62c3b7ec94b
Eric Siebeneich <eric.siebeneich@gmail.com>
2013-11-14 18:18:12 -0600

    Terrain and uav

---
+++ Assets/Custom Assets/Scripts/Terrain/PaintSplatMap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PaintSplatMap : MonoBehaviour {
+
+    List<GameObject> Paths;
+
+	// Use this for initialization
+	void Start () {
+
+        foreach (GameObject path in Paths)
+        {
+            NodeList nodes = path.GetComponent<NodeList>();
+
+            for (int i = 1; i < nodes.Nodes.Length; i++)
+            {
+                
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
