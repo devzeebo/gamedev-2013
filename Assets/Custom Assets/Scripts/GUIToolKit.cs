2e27ed41a6089f5c44fb67124c074dfb4104f364
James Garcia <cooljj_20@sbcglobal.net>
2013-12-03 17:03:25 -0600

    Menu

---
+++ Assets/Custom Assets/Scripts/GUIToolKit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public static class GuiToolkit
+{
+    private static GUIStyle labelStyle;
+    
+	public static Rect Rect(float x, float y, float w, float h)
+    {
+        return new Rect(x, Screen.height - y, w, h);
+    }
+    
+	public static void Label(Vector2 pos, string text)
+    {
+        if (labelStyle == null)
+        {
+            labelStyle = new GUIStyle(GUI.skin.label);
+            labelStyle.clipping = TextClipping.Overflow;
+            labelStyle.wordWrap = false;
+            labelStyle.padding = new RectOffset(4, 0, 0, 0);
+        }
+        GUI.Label(new Rect(pos.x, pos.y, 3, 0), text, labelStyle);
+    }
+    
+	public static float CenterWidth(float width)
+    {
+        return Screen.width / 2 - width / 2;
+    }
+}
---
+++ Assets/Custom Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MainMenu : MonoBehaviour {
+	
+	bool level = false;
+	bool isMainMenu = true;
+	
+	void OnGUI () 
+	{	
+		
+		if(isMainMenu)
+		{
+			// Make a background box
+			GUI.Box(new Rect(Screen.width/2-20,Screen.height/2,100,90), "Fluff Command");
+	
+			// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+			if(GUI.Button(new Rect(Screen.width/2-10,Screen.height/2+30,80,20), "Levels")) 
+			{
+				level = true;
+				isMainMenu = false;
+			}
+	
+			// Make the second button.
+			if(GUI.Button(new Rect(Screen.width/2-10,Screen.height/2+60,80,20), "Quit"))  
+			{
+				Application.Quit();
+				isMainMenu = false;
+			}
+		}
+		else
+		{
+			if(level)
+			{ 	
+				if (GUI.Button(new Rect(Screen.width/2-10,Screen.height/2+30,80,20), "Levels 1"))
+				{
+					Application.LoadLevel(1);
+				}
+				if (GUI.Button(new Rect(Screen.width/2-10,Screen.height/2+60,80,20), "Level 2"))
+				{
+					print ("This is where I would put level 2.... If I had one");
+				}
+			}
+		}
+	}	
+}
