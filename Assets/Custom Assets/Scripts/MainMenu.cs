5ef7661b07e3bd1e7b29e3787fce7ef035f53658
James Garcia <cooljj_20@sbcglobal.net>
2013-12-05 13:55:58 -0600

    Menu Temp-Final
    Final for presentation

--- Assets/Custom Assets/Scripts/MainMenu.cs
+++ Assets/Custom Assets/Scripts/MainMenu.cs
@@ -18,14 +18,14 @@ public class MainMenu : MonoBehaviour {
 		{
 	
 			// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-			if(GUI.Button(new Rect(0,0,Screen.width/3,Screen.height/3), "Levels")) 
+			if(GUI.Button(new Rect(Screen.width/3,Screen.height/5,Screen.width/3,Screen.height/4), "Levels")) 
 			{
 				level = true;
 				isMainMenu = false;
 			}
 	
 			// Make the second button.
-			if(GUI.Button(new Rect(0,0,Screen.width/3,Screen.height/3), "Quit"))  
+			if(GUI.Button(new Rect(Screen.width/3,Screen.height/2,Screen.width/3,Screen.height/4), "Quit"))  
 			{
 				Application.Quit();
 				isMainMenu = false;
@@ -36,11 +36,11 @@ public class MainMenu : MonoBehaviour {
 		{
 			if(level)
 			{ 	
-				if (GUI.Button(new Rect(Screen.width/2-10,Screen.height/2+30,80,20), "Levels 1"))
+				if (GUI.Button(new Rect(Screen.width/3,Screen.height/5,Screen.width/3,Screen.height/4), "Level 1"))
 				{
 					Application.LoadLevel("Level");
 				}
-				if (GUI.Button(new Rect(Screen.width/2-10,Screen.height/2+60,80,20), "Level 2"))
+				if(GUI.Button(new Rect(Screen.width/3,Screen.height/2,Screen.width/3,Screen.height/4), "Level 2")) 
 				{
 					print ("This is where I would put level 2.... If I had one");
 				}
