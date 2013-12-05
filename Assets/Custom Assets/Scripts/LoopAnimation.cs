4c0a47d7e9174bc9b13813da5b6eaaf04283f540
Eric Siebeneich <eric.siebeneich@gmail.com>
2013-12-05 17:20:52 -0600

    Fox animation

---
+++ Assets/Custom Assets/Scripts/LoopAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopAnimation : MonoBehaviour {
+
+	public string AnimationName;
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		animation [AnimationName].speed = Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
